97928a7aa0dd1f68d2ca5d3327e52940d8eef687
Prajwal Basnet <prajalbasnet69@gmail.com>
2025-12-17 10:22:32 +0545

    Perform CRUD operations using database mongodb compass with Nodejs & mongoose

--- server.js
+++ server.js
@@ -1,14 +1,58 @@
-import http from 'http';
-import userschema from './'
+import express from 'express';
+import userModel from './usermodel.js';
 
-const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+app.get("/",(req,res) =>{
+    res.send("This is Home page");
+})
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
-}); 
+// app.get('/getUsers', async (req,res) => {
+//   const user = await userModel.create({
+//     username : "prajwal",
+//     password: "962954"  
+//   })
+//   res.send(user);
+// })
+
+
+// app.get('/updateUser', async(req,res) => {
+//   const updatedUser = await userModel.findOneAndUpdate(
+//     {username: "prajwal"}, //kaslai update garen
+//     {username: "Susmita" }, //k update garne
+//     {new:true} //updated data return garne
+//   )
+// res.send(updatedUser);
+//  })
+
+
+//READ OPERATION
+//find, findOne, findById
+
+// app.get('/readUser', async (req,res) => {
+//   const user = await userModel.findOne(
+// //find
+//     // {username : "prajwal"},
+
+//     {username : "prajwal"},
+//     {username:1, _id:0},
+// )
+//   res.send(user);
+// })
+
+
+// app.get('/deleteUser/:id', async(req,res) => {
+app.get('/deleteUser', async(req,res) => {
+  //findOneAndDelete, findByIdAndDelete
+  const deletedUser = await userModel.findOneAndDelete(
+    // req.params.id
+    // {username: "Susmita"}, //kaslai delete garen
+  )
+res.send(deletedUser);
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port http://localhost:3000");
+})
--- usermodel.js
+++ usermodel.js
@@ -1,98 +1,13 @@
-// import mongoose from 'mongoose';
-
-// // 1. Connect MongoDB
-// mongoose.connect('mongodb://localhost:27017/demo')
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.log(err));
-
-// // 2. Define schema
-// const productschema = mongoose.Schema({
-//   name: String,
-//   price: Number,
-// });
-
-// // 3. Create model
-// const productmodel = mongoose.model('product', productschema);
-
-// // 4. Export model
-// export default productmodel;
-
-// import mongoose from 'mongoose';
-
-// // Connect
-// mongoose.connect('mongodb://localhost:27017/demo');
-
-// // Define schema
-// const productschema = mongoose.Schema({
-//   name: String,
-//   price: Number,
-// });
-
-// // Create model
-// const productmodel = mongoose.model('product', productschema);
-
-// // अब यो file भित्रै productmodel प्रयोग गर्न सकिन्छ
-// async function createProduct() {
-//   await productmodel.create({ name: "Laptop", price: 50000 });
-// }
-
-// createProduct();
-
-
-// import mongoose from 'mongoose';
-
-// mongoose.connect('mongodb://localhost:27017/demo')
-// .then(() => console.log("mongodb connected"))
-// .catch((err) => console.log(err));
-
-// //userschema
-// const userschema = mongoose.Schema({
-//     name : String,
-//     age : Number,
-//     email : String,
-// })
-
-// //usermodel
-// const usermodel = mongoose.model('user', userschema);
-
-// async function createUser(){
-//     await usermodel.create({name: "sushiii", age : "25", email : "sushi@gmail.com"});
-// }
-// createUser();
-
-// export default usermodel;
-
-
+import express from 'express';
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/demo')
-.then(() => console.log("mongodb connected"))
-.catch((err) => console.log(err));
+mongoose.connect('mongodb://localhost:27017/hello')
 
-//userschema
-const userschema = mongoose.Schema({
-    name : String,
-    age : Number,
-    email : String,
-})
+const userSchema = mongoose.Schema({
+    username: String,
+    password: String
+});
 
-//usermodel
-const usermodel = mongoose.model('user', userschema);
+const userModel = mongoose.model("User", userSchema);
 
-//read operation
-async function readUser() {
-    try {
-        const users = await usermodel.find();
-        console.log("Sabai user vetiyo");
-        users.forEach(user => {
-          console.log(`name: ${user.name}, age: ${user.age}, email: ${user.email}`);
-        })
-    } catch (error) {
-        console.log("Error in reading users:", error);
-    }
-   mongoose.connection.close(); 
-}
-readUser();
-
-
-
+export default userModel;
